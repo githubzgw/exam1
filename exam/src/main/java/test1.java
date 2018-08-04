9c3859290453caea481e6b12d3cc15470456f494
20658 <guowei.zhao@hand-china.com>
2018-08-04 13:52:14 +0800

    提交修改信息

--- exam/src/main/java/test1.java
+++ exam/src/main/java/test1.java
@@ -1,34 +1,32 @@
-import java.util.Scanner;
+﻿import java.util.Scanner;
 
 /**
  * Created by Zhao Guowei on 2018/8/4.
  */
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("请输入工资:");
-        Scanner scanner = new Scanner(System.in);
-        double a = Double.parseDouble(scanner.nextLine());
-        double b = 0;
-        if(a > 1500){
-            b = 1500*0.03+(a-1500)*0.1;
+        while(true){
+            System.out.println("请输入工资:");
+            Scanner scanner = new Scanner(System.in);
+            double a = Double.parseDouble(scanner.nextLine())-3500;
+            double b = 0;
+            if (a>=0 && a<1500){
+                b = a *0.03;
+            }else if (a>=1500 && a<4500){
+                b = a * 0.1 - 105;
+            } else if(a>=4500 && a<9000){
+                b = a * 0.2 - 555;
+            }else if(a>=9000 && a<35000){
+                b = a*0.25 - 1005;
+            }else if(a>=35000 && a<55000){
+                b = a*0.3 -2755;
+            }else if(a>=55000 && a<80000){
+                b = a*0.35 - 5505;
+            }else if(a>=80000){
+                b = a*0.45 - 13505;
+            }
             System.out.println("所需要缴纳的税费为:"+b);
-        }else if(a>4500){
-            b = 1500*0.03+3000*0.1+(a-4500)*0.2;
-            System.out.println("所需要缴纳的税费为:"+b);
-        }else if(a>9000){
-            b = 1500*0.03+3000*0.1+4500*0.1+(a-9000)*0.25;
-            System.out.println("所需要缴纳的税费为:"+b);
-        }else if(a>35000){
-            b = 1500*0.03+3000*0.1+4500*0.1+26000*0.2+(a-35000)*0.3;
-            System.out.println("所需要缴纳的税费为:"+b);
-        }else if(a>55000){
-            b = 1500*0.03+3000*0.1+4500*0.1+26000*0.2+20000*0.25+(a-55000)*0.35;
-            System.out.println("所需要缴纳的税费为:"+b);
-        }else if(a>80000) {
-            b = 1500*0.03+3000*0.1+4500*0.1+26000*0.2+20000*0.25+25000*0.35+(a-80000)*0.45;
-            System.out.println("所需要缴纳的税费为:"+b);
-        }else {
-            System.out.println("所需要缴纳的税费为:"+a*0.03);
+
         }
     }
 }
